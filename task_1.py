d7da2d3b3acf927aac405e547be85eed0ed0622a
Oleksandr Knyaz <lichknyaz@gmail.com>
2025-05-06 04:43:24 +0200

    typing and logging

--- task_1.py
+++ task_1.py
@@ -1,61 +1,62 @@
 from abc import ABC, abstractmethod
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 class Vehicle(ABC):
-    def __init__(self, make, model, spec):
+    def __init__(self, make: str, model: str, spec: str) -> None:
         self.make = make
         self.model = model
         self.spec = spec
 
     @abstractmethod
-    def start_engine(self):
+    def start_engine(self) -> None:
         pass
 
 
 class Car(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} {self.spec}: Двигун запущено")
+    def start_engine(self) -> None:
+        logging.info(f"{self.make} {self.model} ({self.spec}): Двигун запущено")
 
 
 class Motorcycle(Vehicle):
-    def start_engine(self):
-        print(f"{self.make} {self.model} {self.spec}: Мотор заведено")
+    def start_engine(self) -> None:
+        logging.info(f"{self.make} {self.model} ({self.spec}): Мотор заведено")
 
 
 class VehicleFactory(ABC):
     @abstractmethod
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         pass
 
     @abstractmethod
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         pass
 
 
-class USVehicleFactory(VehicleFactory, ABC):
-    def create_car(self, make, model):
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "US spec")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "US spec")
 
 
-class EUVehicleFactory(VehicleFactory, ABC):
-    def create_car(self, make, model):
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "EU spec")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "EU spec")
 
 
 # Використання
 
-# США
-us_vehicle = USVehicleFactory()
-us_car1 = us_vehicle.create_car("Ford", "Mustang")
-us_car1.start_engine()
+us_factory = USVehicleFactory()
+us_car = us_factory.create_car("Ford", "Mustang")
+us_car.start_engine()
 
-# ЄС
-eu_vehicle = EUVehicleFactory()
-eu_moto = eu_vehicle.create_motorcycle("BMW", "R1250")
-eu_moto.start_engine()
+eu_factory = EUVehicleFactory()
+eu_bike = eu_factory.create_motorcycle("BMW", "R1250")
+eu_bike.start_engine()
--- task_2.py
+++ task_2.py
@@ -1,65 +1,68 @@
 from abc import ABC, abstractmethod
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author, year) -> None:
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
 
 
 class LibraryInterface(ABC):
 
     @abstractmethod
-    def add_book(self, title, author, year):
+    def add_book(self, title, author, year) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title):
+    def remove_book(self, title) -> None:
         pass
 
     @abstractmethod
-    def show_books(self):
+    def show_books(self) -> list[Book]:
         pass
 
 
 class Library(LibraryInterface):
-    def __init__(self):
+    def __init__(self) -> None:
         self.books: list[Book] = []
 
-    def add_book(self, title, author, year):
+    def add_book(self, title, author, year) -> None:
         self.books.append(Book(title, author, year))
 
-    def remove_book(self, title):
+    def remove_book(self, title) -> None:
         self.books = [book for book in self.books if book.title != title]
 
-    def show_books(self):
+    def show_books(self) -> list[Book]:
         return self.books
 
 
 class LibraryManager:
-    def __init__(self, library: LibraryInterface):
+    def __init__(self, library: LibraryInterface) -> None:
         self.library = library
 
-    def add_book(self, title: str, author: str, year: str):
+    def add_book(self, title: str, author: str, year: str) -> None:
         self.library.add_book(title, author, year)
 
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         self.library.remove_book(title)
 
-    def show_books(self):
-        books = self.library.show_books()
+    def show_books(self) -> None:
+        books: list[Book] = self.library.show_books()
         if not books:
-            print("Library is empty.")
+            logging.info("Library is empty.")
         for book in books:
-            print(book)
+            logging.info(book)
 
 
 def main():
-    library = Library()
+    library: LibraryInterface = Library()
     manager = LibraryManager(library)
 
     while True:
@@ -79,7 +82,7 @@ def main():
             case "exit":
                 break
             case _:
-                print("Invalid command. Please try again.")
+                logging.info("Invalid command. Please try again.")
 
 
 if __name__ == "__main__":
